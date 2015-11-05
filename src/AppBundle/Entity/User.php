add0e96a1608d8cc1b1b474f6f288fdd671f5c6f
Vecliuc Daniel <Vecliuc Daniel>
2015-11-05 18:03:21 +0200

    adaugare

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vecliuc
+ * Date: 11/5/2015
+ * Time: 5:39 PM
+ */
+
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
